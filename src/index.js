a10434cfe67f8af77a1ff376098fbada2fbe4c56
SuXiZheng <freestyle.sago@outlook.com>
2018-09-03 14:36:58 +0800

    增加react-router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Route, Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+const browserHistory = createBrowserHistory();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
